cb4b1862314f6b9bfd03526641e2d234b72d9be0
AhmedMahmoud <ahmed.m.web.dev@gmail.com>
2019-12-26 10:50:44 +0400

    API-integrate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 import "./App.css";
 const APP_ID = "7fbe5d7f";
-const APP_KEY = "1f87ac96920e8b9b590df8a6fe383692	—";
-
+const APP_KEY = "1f87ac96920e8b9b590df8a6fe383692";
 function App() {
+  const [recipe, setRecipe] = useState([]);
+  useEffect(() => {
+    getAllRecipes();
+  }, []);
+  const getAllRecipes = async (search = "chicken") => {
+    const recipes = await axios.get(
+      `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    setRecipe(recipes.data.hits);
+  };
+  const recipeImage = recipe.map(element => {
+    console.log(element.image);
+    return <img src={element.recipe.image} alt="" />;
+  });
   return (
     <div className="App">
-      <p>RecipeApp</p>
+      <p onClick={() => getAllRecipes("banana")}>RecipeApp</p>
+      {recipeImage}
     </div>
   );
 }
