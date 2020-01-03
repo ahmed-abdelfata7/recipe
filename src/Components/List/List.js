769363923ade198f8ad6b10400e0ccea447468aa
AhmedMahmoud <ahmed.m.web.dev@gmail.com>
2020-01-04 00:08:20 +0400

    styling-project

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,10 +1,32 @@
 import React from "react";
+import "./List.css";
 const List = props => {
   let key = 0;
-  const recipeImage = props.recipe.map(element => {
+  const recipeDetails = props.recipe.map(element => {
     key++;
-    return <img src={element.recipe.image} alt="" key={key} />;
+    return (
+      <div className="list-card" key={key}>
+        <div className="list-card-image">
+          <img src={element.recipe.image} alt="" />
+        </div>
+        <div className="list-card-content">
+          <h2>{element.recipe.label}</h2>
+          <p>
+            Hello this is a data from displayed view again and again Hello this
+            is a data from displayed view again and again
+          </p>
+          <a
+            className="btn"
+            href={element.recipe.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read More
+          </a>
+        </div>
+      </div>
+    );
   });
-  return <div>{recipeImage}</div>;
+  return <div className="list-main">{recipeDetails}</div>;
 };
 export default List;
